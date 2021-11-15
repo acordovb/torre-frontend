fc00d43ef9a161f0e9b6ef5d261312b54a112482
Alex Cordova <33164007+alealcor@users.noreply.github.com>
2021-11-14 22:33:43 -0500

    Update People.vue

--- src/views/People.vue
+++ src/views/People.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="People">
-    <h1>This is an People page</h1>
-  </div>
+  <v-card
+    class="mx-auto"
+    max-width="400"
+  >
+    <v-img
+      class="white--text align-end"
+      height="200px"
+      src="https://cdn.alfabetajuega.com/wp-content/uploads/2018/10/167687.alfabetajuega-problemas-tecnicos.jpg"
+    />
+
+    <v-card-subtitle class="pb-0">
+      Sorry! This does not work
+    </v-card-subtitle>
+
+    <v-card-text class="text--primary">
+      <div>We are still working on this page!</div>
+      <div>Come back soon! 🖐</div>
+    </v-card-text>
+  </v-card>
 </template>
